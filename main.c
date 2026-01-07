f12f6502ce03c376b5a93c97662a162e822bd115
CaptainTriton10 <captaintriton167@gmail.com>
2026-01-07 21:13:54 +0000

    feat: added code for camera + simple gradient

--- main.c
+++ main.c
@@ -1,9 +1,26 @@
 #include "raylib.h"
 
+// void CalculatePixelZero(float outArr[3], float resolution[2]) {
+//     Vector3 cameraCenter = {0.0f, 0.0f, 0.0f};
+
+//     float viewportheight = 2.0;
+//     float viewportWidth = viewportheight * (resolution[0] / resolution[1]);
+
+//     Vector3 viewportU = {viewportWidth, 0.0f, 0.0f};
+//     Vector3 viewportV = {0.0, -viewportheight, 0.0f};
+
+//     Vector3 pixelDeltaU = viewportU / resolution[0];
+//     Vector3 pixelDeltaV = viewportV / resolution[1];
+// }
+
 int main(void) {
     // 1. Initialize Window
-    const int screenWidth = 512;
-    const int screenHeight = 512;
+
+    const float aspectRatio = 2.0f / 1.0f;
+
+    const int screenWidth = 2000;
+    const int screenHeight = (int)(screenWidth / aspectRatio);
+
     InitWindow(screenWidth, screenHeight, "Simple Raytracer");
 
     SetTargetFPS(24);
@@ -14,13 +31,28 @@ int main(void) {
         int timeLoc = GetShaderLocation(shader, "time");
         int resLoc  = GetShaderLocation(shader, "resolution");
 
+        int flenLoc = GetShaderLocation(shader, "focalLength");
+        int camCenLoc = GetShaderLocation(shader, "cameraCenter");
+        int viewpLoc = GetShaderLocation(shader, "viewport");
+
+        float viewportHeight = 2.0f;
+
+        float focalLength = 1.0f;
+        float cameraCenter[3] = {0.0f, 0.0f, 0.0f};
+
         while (!WindowShouldClose())
         {
             float time = GetTime();
             float res[2] = { (float)GetScreenWidth(), (float)GetScreenHeight() };
 
+            float viewport[2] = {viewportHeight, viewportHeight*((float)screenWidth / (float)screenHeight)};
+
             SetShaderValue(shader, timeLoc, &time, SHADER_UNIFORM_FLOAT);
             SetShaderValue(shader, resLoc, res, SHADER_UNIFORM_VEC2);
+
+            SetShaderValue(shader, flenLoc, &focalLength, SHADER_UNIFORM_FLOAT);
+            SetShaderValue(shader, camCenLoc, cameraCenter, SHADER_UNIFORM_VEC3);
+            SetShaderValue(shader, viewpLoc, viewport, SHADER_UNIFORM_VEC3);
 
             BeginDrawing();
                 BeginShaderMode(shader);
--- raytracing.frag
+++ raytracing.frag
@@ -3,8 +3,53 @@
 out vec4 finalColour;
 uniform vec2 resolution;
 
-void main() {
-    vec2 uv = gl_FragCoord.xy / resolution;
+uniform float focalLength;
+// uniform vec3 cameraCenter;
+// uniform vec2 viewport;
 
-    finalColour = vec4(uv.x, uv.y, 0.0, 1.0);
+struct Ray {
+    vec3 origin;
+    vec3 direction;
+};
+
+vec4 RayColour(Ray ray) {
+    vec3 unitDirection = normalize(ray.direction);
+    float a = 0.5 * (unitDirection.y + 1.0f);
+
+    vec3 colour = mix(
+            vec3(1.0, 1.0, 1.0),
+            vec3(0.5, 0.7, 1.0),
+            a
+        );
+
+    return vec4(colour, 1.0);
+}
+
+vec3 At(Ray ray, float t) {
+    return ray.origin + ray.direction * t;
+}
+
+void main() {
+    vec3 cameraCenter = vec3(0.0, 0.0, 0.0);
+
+    vec2 pixelIndex = gl_FragCoord.xy - vec2(0.5);
+
+    float viewportHeight = 2.0;
+    float viewportWidth = viewportHeight * (resolution.x / resolution.y);
+
+    vec3 viewportU = vec3(viewportWidth, 0.0, 0.0);
+    vec3 viewportV = vec3(0.0, viewportHeight, 0.0);
+
+    vec3 pixelDeltaU = viewportU / resolution.x;
+    vec3 pixelDeltaV = viewportV / resolution.y;
+
+    vec3 viewportUpperLeft = cameraCenter - vec3(0.0, 0.0, focalLength) - viewportU / 2 - viewportV / 2;
+    vec3 pixel00Loc = viewportUpperLeft + 0.5 * (pixelDeltaU + pixelDeltaV);
+
+    vec3 pixelCenter = pixel00Loc + pixelIndex.x * pixelDeltaU + pixelIndex.y * pixelDeltaV;
+    vec3 rayDirection = pixelCenter - cameraCenter;
+
+    Ray ray = Ray(cameraCenter, rayDirection);
+
+    finalColour = RayColour(ray);
 }
