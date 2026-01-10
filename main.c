ef6971dcb94b07791bc589440409742b0a93a412
CaptainTriton10 <captaintriton167@gmail.com>
2026-01-10 10:43:53 +0000

    refactor: use Camera3D for camera info

--- main.c
+++ main.c
@@ -19,41 +19,59 @@
 #endif
 
 #define CAMERA_MOVE_SPEED 1.5
+#define CAMERA_ZOOM_SPEED 4
 
-void Movement(float cameraCenter[3]) {
+void Movement(Camera *camera) {
     float move = CAMERA_MOVE_SPEED * GetFrameTime();
 
     if (IsKeyDown(KEY_W)) {
-        cameraCenter[2] += -move;
+        camera->position.z += -move;
     }
 
     if (IsKeyDown(KEY_A)) {
-        cameraCenter[0] += -move;
+        camera->position.x += -move;
     }
 
     if (IsKeyDown(KEY_S)) {
-        cameraCenter[2] += move;
+        camera->position.z += move;
     }
 
     if (IsKeyDown(KEY_D)) {
-        cameraCenter[0] += move;
+        camera->position.x += move;
     }
 
     if (IsKeyDown(KEY_LEFT_CONTROL) || IsKeyDown(KEY_C)) {
-        cameraCenter[1] += -move;
+        camera->position.y += -move;
     }
 
     if (IsKeyDown(KEY_SPACE)) {
-        cameraCenter[1] += move;
+        camera->position.y += move;
     }
 }
 
-void DrawInfo(float cameraCenter[3]) {
+void Zoom(Camera *camera) {
+    float zoomFactor = CAMERA_ZOOM_SPEED * GetFrameTime();
+    float scroll = GetMouseWheelMove();
+
+    if (camera->fovy > 0.1 && scroll < 0) {
+        camera->fovy *= 1 + scroll * zoomFactor;
+    }
+
+    if (camera->fovy < 30 && scroll > 0) {
+        camera->fovy *= 1 + scroll * zoomFactor;
+    }
+}
+
+void DrawInfo(Camera camera) {
     // Camera position
     char cameraPosInfo[128];
-    sprintf(cameraPosInfo, "Camera Position: [%.2f, %.2f, %.2f]", cameraCenter[0], cameraCenter[1], cameraCenter[2]);
+    sprintf(cameraPosInfo, "Camera Position: [%.2f, %.2f, %.2f]", camera.position.x, camera.position.y, camera.position.z);
+
+    char cameraFovyInfo[64];
+    sprintf(cameraFovyInfo, "Camera Focal Length: %.2f", camera.fovy);
 
     DrawText(cameraPosInfo, 5, 25, 20, RED);
+    DrawText(cameraFovyInfo, 5, 50, 20, RED);
 }
 
 int main(void) {
@@ -64,7 +82,11 @@ int main(void) {
 
     InitWindow(screenWidth, screenHeight, "Simple Raytracer");
 
-    float cameraCenter[3] = {0.0f, 0.0f, 2.0f};
+    Camera camera = {
+        .position = {0.0f, 0.0f, 2.0f},
+        .fovy = 2.0f
+    };
+
     SetTargetFPS(100);
 
     Shader shader = LoadShader(0, "raytracing.frag");
@@ -78,30 +100,27 @@ int main(void) {
 
     while (!WindowShouldClose()) {    // Detect window close button or ESC key
 
-        float viewportHeight = 2.0f;
 
-        float focalLength = 2.0f;
-
-        Movement(cameraCenter);
+        Movement(&camera);
+        Zoom(&camera);
 
         float time = GetTime();
         float res[2] = { (float)GetScreenWidth(), (float)GetScreenHeight() };
 
-        float viewport[2] = {viewportHeight, viewportHeight*((float)screenWidth / (float)screenHeight)};
-
         SetShaderValue(shader, timeLoc, &time, SHADER_UNIFORM_FLOAT);
         SetShaderValue(shader, resLoc, res, SHADER_UNIFORM_VEC2);
 
-        SetShaderValue(shader, flenLoc, &focalLength, SHADER_UNIFORM_FLOAT);
-        SetShaderValue(shader, camCenLoc, cameraCenter, SHADER_UNIFORM_VEC3);
-        SetShaderValue(shader, viewpLoc, viewport, SHADER_UNIFORM_VEC3);
+        float pos[3] = {camera.position.x, camera.position.y, camera.position.z};
+
+        SetShaderValue(shader, flenLoc, &camera.fovy, SHADER_UNIFORM_FLOAT);
+        SetShaderValue(shader, camCenLoc, pos, SHADER_UNIFORM_VEC3);
 
         BeginDrawing();
             BeginShaderMode(shader);
                 DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), WHITE);
             EndShaderMode();
             DrawFPS(5, 5);
-            DrawInfo(cameraCenter);
+            DrawInfo(camera);
         EndDrawing();
     }
 
